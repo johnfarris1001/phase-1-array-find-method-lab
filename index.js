66c0f4ca9ad8843f1ec7e6df43f3ccaac31b1955
John <johnfarris217@gmail.com>
2022-11-29 18:02:17 -0600

    commit tree 64091415ad06d70536804aced02bf1021f983307

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+
+
+
+function superbowlWin(array) {
+    if (array.find(array => array.result === "W") === undefined) {
+        return undefined
+    }
+    return array.find(array => array.result === "W").year;
+}
+
